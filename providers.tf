f99864a04d290d28061a2361f69a1baa766d162c
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2024-12-10 09:17:39 +0000

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -6,3 +6,34 @@ terraform {
     }
   }
 }
+
+resource "fastly_service_v1" "demo" {
+  name = "demofastly"
+
+  domain {
+    name    = "demo.notexample.com"
+    comment = "demo"
+  }
+
+  backend {
+    address = "127.0.0.1"
+    name    = "localhost"
+    port    = 80
+  }
+
+  force_destroy = true
+}
+
+terraform {
+  required_providers {
+    fastly = {
+      source = "fastly/fastly"
+      version >= "0.38.0"
+    }
+  }
+}
+
+# Configure the Fastly Provider
+provider "fastly" {
+  api_key = "test"
+}
